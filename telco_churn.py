eef60047272080495ed5f3f167689a3cf9ca71da
Emmanuel Azua <emmanuel.a.rivera.a@gmail.com>
2021-04-29 17:46:44 -0500

    First Visualization Project Update

--- telco_churn.py
+++ telco_churn.py
@@ -44,7 +44,7 @@ url = get_db_url(username, password, host)
 # In[4]:
 
 
-tables = pd.read_sql("SHOW TABLES", url)
+tables = pd.read_sql("SHOW TABLES", url).drop(columns=['multiple_lines', 'C'])
 tables
 
 
@@ -135,14 +135,14 @@ sns.heatmap(pearsoncorr,
             linewidth = 0.5)
 
 
-# In[20]:
+# In[17]:
 
 
 fig, first = plt.subplots(figsize=(15, 15))
 first = sns.scatterplot(data = customers, x = "tenure", y = "payment_type_id", hue = "churn")
 
 
-# In[24]:
+# In[18]:
 
 
 sns.relplot(data = customers, x = "tenure", y = "contract_type_id", hue = "churn")
@@ -154,46 +154,208 @@ sns.relplot(data = customers, x = "tenure", y = "contract_type_id", hue = "churn
 sns.violinplot(data = customers, x = "payment_type_id", y = "contract_type_id", hue = "churn")
 
 
-# In[30]:
+# In[20]:
 
 
 sns.pointplot(data = customers, x = "streaming_movies", y = "senior_citizen", hue = "churn")
 
 
-# In[31]:
+# In[21]:
 
 
 sns.relplot(data = customers, x = "tenure", y = "senior_citizen", hue = "churn")
 
 
-# In[33]:
+# In[22]:
 
 
 sns.lineplot(data = monthly_customers, x = "payment_type_id", y = "churn")
 
 
-# In[ ]:
+# In[23]:
 
 
 yearly_customers = customers[customers["contract_type_id"] == 2]
 yearly_customers
 
 
-# In[ ]:
+# In[24]:
 
 
 two_year_customers = customers[customers["contract_type_id"] == 3]
 two_year_customers
 
 
-# In[ ]:
+# In[25]:
 
 
 sns.pairplot(data = monthly_customers)
 
 
-# In[ ]:
+# In[46]:
 
 
+spreadsheet = pd.read_excel("Spreadsheets_Exercises_Solutions.xlsx", sheet_name = "mytable_customer_details_values", header = 1)
+spreadsheet
 
+
+# In[47]:
+
+
+spreadsheet.info()
+
+
+# In[49]:
+
+
+pearsoncorr = spreadsheet.corr(method = 'pearson')
+pearsoncorr
+
+
+# In[53]:
+
+
+plt.figure(figsize = (16,5))
+sns.heatmap(pearsoncorr, 
+            xticklabels = pearsoncorr.columns,
+            yticklabels = pearsoncorr.columns,
+            cmap = 'RdBu_r',
+            annot = True,
+            linewidth = 0.5,
+           center = 0)
+
+
+# In[55]:
+
+
+sns.pairplot(data = spreadsheet)
+
+
+# In[61]:
+
+
+sns.relplot(data = spreadsheet, x = "tenure", y = "average_monthly_charges", hue = "churn")
+
+
+# In[123]:
+
+
+monthly_customers = spreadsheet[spreadsheet["contract_type"] == 0]
+monthly_customers
+
+
+# In[137]:
+
+
+yearly_customers = spreadsheet[spreadsheet["contract_type"] == 1]
+yearly_customers
+
+
+# In[138]:
+
+
+twoyear_customers = spreadsheet[spreadsheet["contract_type"] == 2]
+twoyear_customers
+
+
+# In[124]:
+
+
+sns.pairplot(data = monthly_customers)
+
+
+# In[125]:
+
+
+sns.relplot(data = monthly_customers, x = "tenure", y = "average_monthly_charges", hue = "churn")
+
+
+# In[126]:
+
+
+sns.lineplot(data = monthly_customers, x = "payment_type", y = "average_monthly_charges", hue = "churn")
+
+
+# In[127]:
+
+
+sns.lineplot(data = monthly_customers, x = "is_senior_citizen", y = "average_monthly_charges", hue = "churn")
+
+
+# In[128]:
+
+
+pearsons4monthly = monthly_customers.corr(method = 'pearson')
+pearsons4monthly
+
+
+# In[129]:
+
+
+plt.figure(figsize = (16,5))
+sns.heatmap(pearsons4monthly, 
+            xticklabels = pearsoncorr.columns,
+            yticklabels = pearsoncorr.columns,
+            cmap = 'RdBu_r',
+            annot = True,
+            linewidth = 0.5,
+           center = 0)
+
+
+# In[130]:
+
+
+plt.figure(figsize = (25,10))
+sns.swarmplot(data = monthly_customers, x = "tenure", y = "payment_type", hue = "churn", size = 10)
+
+
+# In[131]:
+
+
+plt.figure(figsize = (16,5))
+sns.relplot(data = monthly_customers, x = "tenure", y = "payment_type", hue = "churn")
+
+
+# In[132]:
+
+
+sns.lineplot(data = monthly_customers, x = "payment_type", y = "tenure", hue = "churn")
+
+
+# In[133]:
+
+
+sns.relplot(data = monthly_customers, x = "has_phone", y = "tenure", hue = "churn")
+
+
+# In[134]:
+
+
+sns.relplot(data = monthly_customers, x = "has_internet", y = "tenure", hue = "churn")
+
+
+# In[135]:
+
+
+sns.relplot(data = monthly_customers, x = "has_phone_and_internet", y = "tenure", hue = "churn")
+
+
+# In[141]:
+
+
+sns.relplot(data = monthly_customers, x = "is_senior_citizen", y = "payment_type", hue = "churn")
+
+
+# In[210]:
+
+
+total_churners = spreadsheet.groupby(["has_churned"]).count().iloc[:,14].sort_values(ascending = True).head(1) #[spreadsheet['has_churned'] == True] #monthly_customers[monthly_customers["has_churned" == True]].value_counts()
+total_churners
+
+
+# In[213]:
+
+
+churner_groups = spreadsheet.groupby(["contract_type", "has_churned"]).count().iloc[:,14].sort_values(ascending = True)
+churner_groups
 
